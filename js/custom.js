1af59d5b2044cc78b8ab138fd3aac58d222c8c04
BilliPixel <babasala35@gmail.com>
2022-03-29 15:30:29 +0100

    first commit

--- js/custom.js
+++ js/custom.js
@@ -99,7 +99,7 @@ window.addEventListener("DOMContentLoaded", function () {
   function success() {
     form.reset();
     status.classList.add("success");
-    status.innerHTML = "Thanks!";
+    status.innerHTML = "Thanks";
   }
 
   function error() {
